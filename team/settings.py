f0031808d041d6305b40c797c48d10722cdefb78
yzx <23371389@buaa.edu.cn>
2025-07-03 09:16:52 +0800

    新增图片大小限制

--- team/settings.py
+++ team/settings.py
@@ -15,6 +15,10 @@ import pymysql
 
 pymysql.install_as_MySQLdb()
 
+# 提高上传文件内存限制（单位：字节）
+DATA_UPLOAD_MAX_MEMORY_SIZE = 10 * 1024 * 1024  # 10MB
+FILE_UPLOAD_MAX_MEMORY_SIZE = 10 * 1024 * 1024  # 10MB
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
